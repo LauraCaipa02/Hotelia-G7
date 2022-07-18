3c90641278065391da97f063ed284e9d792298b5
LauraCaipa02 <98822299+LauraCaipa02@users.noreply.github.com>
2022-07-18 11:08:50 -0500

    folder structuring was created

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><i class="fa-solid fa-house"></i>Inicio</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+
+function Home(){
+    return(
+        <main>
+            <Header />
+            <Navbar />
+        </main>
+    )
+}
+export default Home;

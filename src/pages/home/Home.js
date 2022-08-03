1befce48d9d13ad969935a3bc5ac6dd5de68d8f5
Viviana Duarte <yvduartef@gmail.com>
2022-08-02 22:44:24 -0500

    assets route and footer added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import SearchBar from "../../components/SearchBar/SearchRoom";
 import Navbar from "../../components/navbar/Navbar";
 import RoomType from "../../components/SearchBar/RoomType";
+import FooterDerlHeader from "../../components/Footer/FooterDerlHeader";
 function Home(){
     return(
         <main>
             <Navbar />
             <SearchBar />
             <RoomType />
+            <FooterDerlHeader />
         </main>
     )
 }
